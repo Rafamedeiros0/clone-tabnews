841ae1466a514994f80813353ecea8ac6e83f508
Rafael Medeiros <rafamedeiros0@gmail.com>
2026-01-16 09:20:24 -0700

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -2,7 +2,6 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   let client;
-
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
@@ -22,7 +21,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues()
+    ssl: getSSLValues(),
   });
 
   await client.connect();
@@ -38,7 +37,8 @@ function getSSLValues() {
   if (process.env.POSTGRES_CA) {
     return {
       ca: process.env.POSTGRES_CA,
-    }
+    };
   }
-  return process.env.NODE_ENV === "production" ? true : false,
+
+  return process.env.NODE_ENV === "production" ? true : false;
 }
